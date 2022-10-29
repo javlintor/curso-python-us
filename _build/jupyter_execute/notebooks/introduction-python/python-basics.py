66217d000daab1106c120302f4d80268889150ce
Javier Linares <jlinarestorres2@gmail.com>
2022-10-29 22:37:45 +0200

    book restructure

--- _build/jupyter_execute/notebooks/introduction-python/python-basics.py
+++ _build/jupyter_execute/notebooks/introduction-python/python-basics.py
@@ -5,9 +5,6 @@
 # 
 # En esta sección y las siguientes vamos a familiarizarnos con los tipos predefinidos de Python y cómo manejarlos para transformar los diferentes datos de nuestro programa. 
 # 
-# ---
-# ## Predefinidos, Variables y Expresiones
-# 
 # Python incorpora una serie de tipos predefinidos o primitivos como **enteros**, **flotantes**, **cadenas** o **booleanos**
 
 # ```
@@ -39,13 +36,13 @@ x = 42
 type(42)
 
 
-# In[51]:
+# In[ ]:
 
 
 isinstance("foo", str)
 
 
-# In[2]:
+# In[ ]:
 
 
 import math
@@ -113,6 +110,14 @@ print(id(m))
 # ![picture](https://drive.google.com/uc?id=1wI1R_M0z9A8k5ynOnlO8U3FK10wUKZwf)
 # 
 
+# Para comprobar si dos variables referencian el mismo objeto utlizamos la función `is`.
+
+# In[ ]:
+
+
+n is m
+
+
 # Asignando la variable `m` a otro objeto, su referencia en memoria cambiará
 
 # In[ ]:
@@ -120,6 +125,7 @@ print(id(m))
 
 m = 400
 print(id(m))
+print(m is n)
 
 
 # ![picture](https://drive.google.com/uc?id=1L6hU-KtS7T9nZNtx_hc2650Zp5Er3WJ5)
@@ -192,7 +198,7 @@ number_of_college_graduates = 2500     # Snake Case
 # 8 / 5            # 1.6
 # ```
 
-# In[6]:
+# In[ ]:
 
 
 2 + 3 * 4
@@ -200,19 +206,19 @@ number_of_college_graduates = 2500     # Snake Case
 
 # Otras operaciones importantes entre tipos numéricos son el operador potencia `**`, cociente `//` y resto, `%`. 
 
-# In[7]:
+# In[ ]:
 
 
 2 ** 8
 
 
-# In[8]:
+# In[ ]:
 
 
 16 // 3
 
 
-# In[9]:
+# In[ ]:
 
 
 23 % 7
@@ -222,7 +228,7 @@ number_of_college_graduates = 2500     # Snake Case
 # 
 # Por ejemplo, al aplicar el operador suma `+` entre un entero y un flotante, Python convertirá el entero en flotante y aplicará en operador posteriormente.
 
-# In[18]:
+# In[ ]:
 
 
 a = 1
@@ -234,7 +240,7 @@ print(type(a + b))
 
 # Para realizar un **tipado explícito**, llamamos directamente al tipo que queremos convertir 
 
-# In[62]:
+# In[ ]:
 
 
 a = "8"
@@ -244,27 +250,14 @@ print(type(b))
 
 
 # ---
-# ## Cadenas
+# ## Comentarios
+# En Python, podemos comentar nuestro código escribiendo el símbolo `#`, de modo que todo lo que escribamos a la derecha de `#` en la misma línea será ignorado por el intérprete. 
 
-# ---
-# ## El tipo `None`
+# In[24]:
 
-# --- 
-# ## Booleanos 
 
-# ---
-# ## Tipos secuenciales 
+# Esto es un comentario 
+a = 1 + 2 # Esto es un comentario tras una asignación
 
-# ### Listas
 
-# ### Tuplas
-
-# ### Diccionarios
-
-# 
-
-# 
-
-# 
-
-# 
+# Escribir comentarios cuando estemos creando nuestro código es fundamental, sobre todo de cara a que otros (incluyendo nuestro yo del futuro) podamos entenderlo más fácilmente. No obstante, debería ser más importante centrarnos en que nuestro código sea legible que en escribir comentarios para todo. Véase [este blog](https://stackoverflow.blog/2021/12/23/best-practices-for-writing-code-comments/) para buenas prácticas a la hora de escribir comentarios. 
